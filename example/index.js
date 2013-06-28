05ab2cc832a21eca9e26b0c829f4187cb9fe2999
Mitchel Kelonye <kelonyemitchel@gmail.com>
2013-06-29 00:38:21 +0300

    removed jade dependency

--- example/index.js
+++ example/index.js
@@ -1,15 +1,11 @@
 var express = require('express');
-var jade = require('component-hooks/node_modules/jade').__express;
 var app = express();
 
 app.use(express.favicon());
 app.use(express.static(__dirname + '/../public'));
-app.set('views', __dirname + '/views');
-app.engine('jade', jade);
-app.set('view engine', 'jade');
 
 app.get('/', function(req, res){
-  res.render('index');
+  res.sendfile(__dirname + '/index.html');
 });
 
 app.listen(3000);
